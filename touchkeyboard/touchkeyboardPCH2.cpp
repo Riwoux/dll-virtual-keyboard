5e706c77d1192507ac7be7c80bb60f023c7e74cd
Corentin LANGLADE <corentin.langlade@actemium.com>
2026-02-27 15:44:02 +0100

    maj

--- touchkeyboard/touchkeyboardPCH2.cpp
+++ touchkeyboard/touchkeyboardPCH2.cpp
@@ -29,7 +29,6 @@ static bool g_KeyboardVisible = false;
 //---------------------------------------------------------------------------
 
 //fais partis du principe de cacher le clavier lorsque que la souris clique en dehors de la zone
-// Verifie si la souris clique dans la fenetre
 static bool IsPointInsideWindow(HWND hwnd, const POINT& ptScreen)
 {
 	if (!hwnd) return false;
@@ -38,7 +37,6 @@ static bool IsPointInsideWindow(HWND hwnd, const POINT& ptScreen)
 	return PtInRect(&rc, ptScreen) != 0;
 }
 
-// Verifie si la fenetre cliqué est le clavier
 static bool IsInKeyboardWindow(HWND hwnd)
 {
 	if (!hwnd) return false;
@@ -53,7 +51,6 @@ static bool IsInKeyboardWindow(HWND hwnd)
     return (root == kb);
 }
 
-// Verifie le type de d'objet où l'on est rentré
 bool IsTextControl(HWND hwnd)
 {
     wchar_t className[256];
@@ -69,7 +66,6 @@ bool IsTextControl(HWND hwnd)
             wcsstr(className, L"Edit") != NULL);
 }
 
-// Verifie si le type de l'objet est numérique
 bool IsNumericControl(HWND hwnd)
 {
 	LONG style = GetWindowLong(hwnd, GWL_STYLE);
@@ -82,6 +78,7 @@ LRESULT CALLBACK SubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 {
 	switch (uMsg)
 	{
+        // afficher le clavier et le mettre au premier plan
 		case WM_SETFOCUS:
 		{
 			auto it = g_AttachedControls.find(hwnd);
@@ -92,7 +89,7 @@ LRESULT CALLBACK SubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 			}
             break;
         }
-
+        // caché le clavier
         case WM_KILLFOCUS:
 		{
 			if (g_AutoHide) {
@@ -106,7 +103,7 @@ LRESULT CALLBACK SubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
             }
             break;
 		}
-
+        // détruire les gestions de focus
         case WM_DESTROY:
         {
 			RemoveWindowSubclass(hwnd, SubclassProc, uIdSubclass);
@@ -119,7 +116,7 @@ LRESULT CALLBACK SubclassProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 }
 
  //fais partis du principe de cacher le clavier lorsque que la souris clique en dehors de la zone
- //
+ // Recupere le focus en dessous di clique de la souris
 LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
     if (nCode >= 0)
@@ -129,14 +126,14 @@ LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam)
         {
             MOUSEHOOKSTRUCT* mhs = reinterpret_cast<MOUSEHOOKSTRUCT*>(lParam);
             POINT pt = mhs->pt;
-
 			HWND hwndHit = ::WindowFromPoint(pt);
 
+            // si on est dans le clavier -> ne rien faire
 			if (IsInKeyboardWindow(hwndHit)) {
 				return CallNextHookEx(g_MouseHook, nCode, wParam, lParam);
 			}
 
-
+            // si un focus est défini, recuperer ses parametres et afficher le clavier en conséquences
 			if (g_CurrentTarget) {
                 if (IsPointInsideWindow(g_CurrentTarget, pt)) {
                     if (!g_KeyboardVisible) {
@@ -154,25 +151,25 @@ LRESULT CALLBACK MouseHookProc(int nCode, WPARAM wParam, LPARAM lParam)
     return CallNextHookEx(g_MouseHook, nCode, wParam, lParam);
 }
 
+// identifie et mémorise les informations de la fenetre
 BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lParam)
 {
     if (IsTextControl(hwnd)) {
         if (g_AttachedControls.find(hwnd) == g_AttachedControls.end()) {
             SetWindowSubclass(hwnd, SubclassProc, 0, 0);
-
             ControlInfo info;
             info.hwnd = hwnd;
             info.isNumeric = IsNumericControl(hwnd);
-
             g_AttachedControls[hwnd] = info;
         }
     }
-
+    // TRUE -> continue l'énumération
     return TRUE;
 }
-
+// appelé automatiquement par windows lors de l'utilisation de la DLL
 int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved)
 {
+    // lors de l'arret de l'utilisation de la DLL il va enlever ses pocédures de controle des fenetres
     if (reason == DLL_PROCESS_DETACH) {
 		for (auto& pair : g_AttachedControls) {
 			RemoveWindowSubclass(pair.first, SubclassProc, 0);
@@ -184,7 +181,6 @@ int WINAPI DllEntryPoint(HINSTANCE hinst, unsigned long reason, void* lpReserved
                 UnhookWindowsHookEx(g_MouseHook);
                 g_MouseHook = NULL;
         }
-
     }
     return 1;
 }
@@ -207,6 +203,7 @@ extern "C"
                 Application->CreateForm(__classid(Tkeyboard), &keyboard);
             }
 
+            // prend le focus sur le clavier et l'affiche en le mettant au premier plan
             if (keyboard) {
 				g_CurrentTarget = targetHandle; //fais partis du principe de cacher le clavier lorsque que la souris clique en dehors de la zone
                 g_KeyboardHwnd  = keyboard->Handle;
@@ -230,18 +227,16 @@ extern "C"
         }
     }
 
+    // initialise le fonctionnement du jeu de focus entre fenetres
     __declspec(dllexport) void __stdcall KeyboardAttachToForm(HWND formHandle, bool autoHide)
     {
         if (!formHandle || !::IsWindow(formHandle)) {
             MessageBoxW(NULL, L"formHandle invalide!", L"Erreur", MB_OK | MB_ICONERROR);
             return;
         }
-
         g_AutoHide = autoHide;
-
-        // Énumérer tous les contrôles enfants
+        // Énumérer tous les contrôles enfants pour gérer toutes les fenetres
         ::EnumChildWindows(formHandle, EnumChildProc, 0);
-
         //fais partis du principe de cacher le clavier lorsque que la souris clique en dehors de la zone
         if (!g_MouseHook) {
                 g_MouseHook = SetWindowsHookEx(WH_MOUSE, MouseHookProc, NULL, GetCurrentThreadId());
@@ -250,6 +245,7 @@ extern "C"
 
     }
 
+    // Suprimme les controles du jeu de focus entre fenetres
     __declspec(dllexport) void __stdcall KeyboardDetachFromForm(HWND formHandle)
     {
         if (!formHandle || !::IsWindow(formHandle)) return;
@@ -273,6 +269,7 @@ extern "C"
 
     }
 
+    // fonction a appeler lors d'évenement comme OnEnter pour afficher le clavier
     __declspec(dllexport) void __stdcall KeyboardAutoShow(HWND hwnd)
     {
         try {
